d3f2d3403b9992de766369d75ff926435dc2440b
Mikhail Shornikov <mikhail.shornikov@mail.ru>
2024-09-14 09:54:01 +0300

    feat: Добавлят базовый пример

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Editable Tree Demo
 
-Demo of custom tree with combobox leaves
+Demo of custom tree with combobox leaves.
+
+[Link to demo](https://mshornikov.github.io/editable-tree-demo/)
+
+## Running locally
+
+Required dependencies for running locally: [Node.js](https://nodejs.org) and enabled [corepack](https://nodejs.org/api/corepack.html). Corepack used to control package manager.
+
+To enable corepack:
+
+```sh
+corepack enable
+```
+
+As package manager we use [pnpm](https://pnpm.io).
+
+To install dependencies:
+
+```sh
+pnpm i
+```
+
+To run dev server:
+
+```sh
+pnpm run dev
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,61 @@
+import { NodeRendererProps, Tree } from 'react-arborist';
+
+type Data = { id: string; name: string; children?: Data[] };
+
+const data = Array.from({ length: 100 }).map((_, index) => {
+    const object: Data = { id: index.toString(), name: index.toString() };
+
+    if (index < 10) {
+        object.children = Array.from({ length: 100 }).map((_, i) => {
+            const child: Data = {
+                id: `${index.toString()}${i.toString()}`,
+                name: `${index.toString()}${i.toString()}`,
+            };
+
+            if (i < 10) {
+                child.children = [
+                    { id: index.toString(), name: index.toString() },
+                ];
+            }
+
+            return child;
+        });
+    }
+
+    return object;
+});
+
+const options = ['string', 'boolean', 'number', 'object', 'array'];
+
+function Node({ node, style, dragHandle }: NodeRendererProps<Data>) {
+    return (
+        <div
+            style={style}
+            ref={dragHandle}
+            onClick={() => {
+                if (node.isInternal) {
+                    node.toggle();
+                }
+            }}>
+            {!node.isLeaf && '+'}
+            <select>
+                <option value={node.data.name}>{node.data.name}</option>
+                {options.map((i) => (
+                    <option value={i} key={i}>
+                        {i}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
+
 function App() {
-    return <div>Test</div>;
+    return (
+        <Tree initialData={data} openByDefault={false}>
+            {Node}
+        </Tree>
+    );
 }
 
 export { App };
