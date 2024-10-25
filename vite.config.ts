7af049a901b3b205b4763f483b81b6e4cce4c5e5
Mikhail Shornikov <mikhail.shornikov@mail.ru>
2024-10-25 15:28:17 +0300

    chore: Переносит конфиг postcss в vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     base: './',
+    css: {
+        postcss: {
+            plugins: [tailwind, autoprefixer],
+        },
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
